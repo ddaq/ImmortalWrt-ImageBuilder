b71a6c5b52f1c69e360079c0e0d47fb276b49d5a
ddaq <52245497+ddaq@users.noreply.github.com>
2026-09-15 20:39:44 +0800

    Add python3-pip to installation packages

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -47,6 +47,7 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建固件..."
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
+PACKAGES="$PACKAGES python3-pip"
 PACKAGES="$PACKAGES ss"
 PACKAGES="$PACKAGES bash"
 PACKAGES="$PACKAGES python3"
